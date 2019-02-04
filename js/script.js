f250c524d00fc7a5a83f8890c351af05fc058582
Zeyd Alabsi <gotozeyd336@gmail.com>
2019-02-04 23:04:31 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Sexyback", "High Life", "Raging of a Sunday"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var imageLinks = ["https://image.slidesharecdn.com/how-20to-20make-20your-20charts-20sexy-20enough-20for-20jt-131004212308-phpapp02/95/how-to-make-your-charts-sexy-enough-for-justin-timberlake-34-638.jpg?cb=1380961785", "https://direct.rhapsody.com/imageserver/images/Alb.335732394/500x500.jpg", "https://www.google.com/search?safe=strict&rlz=1CANQDY_enUS815&biw=1366&bih=610&tbm=isch&sa=1&ei=wLtYXJiMBNKQsAXz1ISgAQ&q=raging+on+a+sunday&oq=Raging+on+a+&gs_l=img.3.0.0j0i24.72304.76497..77846...0.0..0.90.960.12......2....1..gws-wiz-img.....0..35i39j0i67j0i8i30.ptqfDkWlVhE#imgrc=B-uT-v6QxAOvgM:"];
 
+var artists = ["Justin Timberlake", "Nick Tierre", "Bohnes"];
 
+var songLengths = [400, 330, 330];
+
+var songLinks = ["https://www.youtube.com/watch?v=FdSRfdu_dLU", "https://www.youtube.com/watch?v=_NhkMga2RZ8&list=LLjiNCV4fQfACeVJTUk9VxMg&index=9&t=0s", "https://www.youtube.com/watch?v=zrFhr2U0ytg"];
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
